32dc6be211eb358198e1b592c30d1bb7475c06e7
Annie Xu <axu.1492@gmail.com>
2019-02-06 15:57:45 -0800

    added UI scene

---
+++ WildCard1/Assets/AnnieScripts/ChangeText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = "Hello";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void ChangeThisText(string NewText) {
+		GetComponent<Text>().text = NewText;
+	}
+}
