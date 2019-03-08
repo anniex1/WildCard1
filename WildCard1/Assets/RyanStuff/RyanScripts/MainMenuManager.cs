645eb64425861edf287c616ac719ab026f102f36
rhartant@uci.edu <44073902+rhartant@users.noreply.github.com>
2019-03-08 10:58:18 -0800

    Added main menu scene

---
+++ WildCard1/Assets/RyanStuff/RyanScripts/MainMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject CreditsContainer;
+    public GameObject MainMenuContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenCredits()
+    {
+        MainMenuContainer.SetActive(false);
+        CreditsContainer.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        CreditsContainer.SetActive(false);
+        MainMenuContainer.SetActive(true);
+    }
+
+}
